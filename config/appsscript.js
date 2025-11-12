833d49e125374632e4c2d6e24dd6593ccd63b224
horikei525 <horikei525@gmail.com>
2025-11-12 18:56:07 +0900

    Delete config directory

--- config/appsscript.js
+++
@@ -1,7 +0,0 @@
-window.AppScriptConfig = {
-  /**
-   * Google Apps Script の Web アプリ URL。
-   * 例: https://script.google.com/macros/s/XXXXXXXXXXX/exec
-   */
-  endpoint: 'https://script.google.com/macros/s/your-script-id/exec'
-};
